43ef51a82914493b9a95a9562e962e3123f87fea
Zulfikarlatief <zulfikar_latief@yahoo.com>
2013-06-20 23:53:34 +0700

    first commit

---
+++ src/Kalkulator/Main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Kalkulator;
+
+import Kalkulator.UI.KalkulatorForm;
+
+/**
+ *
+ * @author zword
+ */
+public class Main {
+    public static void main(String[] args) {
+        KalkulatorForm form = new KalkulatorForm();
+        form.setVisible(true);
+    }
+}
